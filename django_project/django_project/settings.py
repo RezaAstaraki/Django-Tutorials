cd079d69240dbacbbf0abc01cdc1e31d55417836
Omidi-a <omidiazam325@gmail.com>
2024-03-13 17:23:54 +0330

    2

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
