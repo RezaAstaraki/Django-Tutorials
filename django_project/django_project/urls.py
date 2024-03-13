c70921b4ac557b88470595991d70c2dc1463bb56
Omidi-a <omidiazam325@gmail.com>
2024-03-13 17:34:55 +0330

    add git ignores

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from users.views import register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('blog.urls'))
+    path('',include('blog.urls')),
+    path('register',register_view,name='register')
 ]
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register_view(request):
+
+    return render(request,'users/register.html')
+
 
 
