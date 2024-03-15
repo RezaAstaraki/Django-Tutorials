ba9fc1aed4fcf0b33dedf8d5400d2bb9647ede15
RezaAstaraki <reza.astaraky@gmail.com>
2024-03-15 12:27:22 +0330

    add logout and login

--- django_project/mysite/mysite/urls.py
+++ django_project/mysite/mysite/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from home.views import home_view
+from django.contrib.auth.views import LogoutView, LoginView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view,name='home'),
-    path('',include('home.urls')),
+    path('', home_view, name='home'),
+    path('login/', LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/',
+         LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    path('', include('home.urls')),
+
 ]
