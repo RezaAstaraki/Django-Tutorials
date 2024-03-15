13c25afccd74ee17936e336782b7637ed4c5f822
RezaAstaraki <reza.astaraky@gmail.com>
2024-03-15 16:55:22 +0330

    add profile link to navbar

--- django_project/mysite/mysite/urls.py
+++ django_project/mysite/mysite/urls.py
@@ -18,16 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from home.views import home_view
 from django.contrib.auth.views import LogoutView, LoginView
-from user.views import register_view
+from user.views import register_view, profile_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
     path('register/', register_view, name='register'),
+    path('profile/', profile_view, name='profile'),
     path('login/', LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/',
          LogoutView.as_view(template_name='user/logout.html'), name='logout'),
     path('', include('home.urls')),
-
 ]
--- django_project/mysite/user/views.py
+++ django_project/mysite/user/views.py
@@ -20,3 +20,8 @@ def register_view(request):
         context = {'form': form}
         print('*********************get')
         return render(request, 'user/register.html', context=context)
+
+
+def profile_view(request):
+
+    return render(request, 'user/profile.html')
